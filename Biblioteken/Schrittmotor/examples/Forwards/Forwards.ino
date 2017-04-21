6e5c6c73ce37e049177216f5801feb6810ab87de
Marcel <Marcel_Mich@web.de>
2017-04-21 20:12:56 +0200

    Update Forwards.ino

--- Biblioteken/Schrittmotor/examples/Forwards/Forwards.ino
+++ Biblioteken/Schrittmotor/examples/Forwards/Forwards.ino
@@ -1,6 +1,6 @@
 #include <Schrittmotor.h>
-
-Schrittmotor Motoren(0, 1, 2, 3, 8, 9, 10, 11);
+byte *PinOut = {0, 1, 2, 3, 8, 9, 10, 11};
+Schrittmotor Motoren(PinOut);
 
 void setup() {
   Motoren.setNewSpeed(3000);
