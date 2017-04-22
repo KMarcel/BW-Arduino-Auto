d1ea414b366eb5ba5be529ecf9090d633d468362
Marcel <Marcel_Mich@web.de>
2017-04-22 15:57:11 +0200

    Update BW-Auto1.ino

--- src/BW-Auto1.ino
+++ src/BW-Auto1.ino
@@ -1,20 +1,31 @@
 #include <BW_Auto.h>
-int Ergebnis = 0;
-int Pins[8] = {0, 1, 2, 3, 8, 9, 10, 11};
-BW_Auto Auto(Pins, 7, 6, 100);
+int Abstand = 0;
+int Pins[8] = {3, 4, 5, 6, 8, 9, 10, 11};
+BW_Auto Auto(Pins, 1, 2, 2000);
 
 void setup() {
   Auto.setNewSpeed(2000);
 }
 
 void loop() {
-  Ergebnis = Auto.distanz();
-  if(Ergebnis > 30)
+  Abstand = Auto.distanz();
+  if(Abstand > 30)
   {
     Auto.Forwards();
   }
-  if(Ergebnis < 30)
+  if(Abstand < 30)
   {
-    Auto.Left();
+    Auto.Backwards(20);
+    Auto.Left(90);
+    Abstand = Auto.distanz();
+    if(Abstand < 50)
+    {
+      Auto.Right(180); 
+      Abstand = Auto.distanz();
+      if(Abstand < 50)
+      {
+        Auto.Right(90);
+      }
+    }     
   }
 }
