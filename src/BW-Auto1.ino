59402dd737704a0a48848cdfb5d6753909d4f89d
Marcel <Marcel_Mich@web.de>
2017-05-05 07:47:06 +0200

    Update BW-Auto1.ino

--- src/BW-Auto1.ino
+++ src/BW-Auto1.ino
@@ -1,32 +1,45 @@
 #include <BW_Auto.h>
 BW_Auto Auto(3, 2, 0, 1, 8, 9, 10, 11, 7, 6, 100);
-unsigned long An = 0;
-
+unsigned long An = 16;
+int Links = 0;
 void setup() {
-  Auto.setNewSpeed(2500);
+  Auto.setNewSpeed(3000);
 }
 
 void loop() {
-    An = Auto.distanz(false);
-    if(An >= 15)
-    {
-      Auto.Forwards();
-    }else
-    {
-      Auto.Backwards(20);
-      Auto.Left(50);
-      delay(1000);
-      An = Auto.distanz(true);
-      if(An < 15)
-      {
-        Auto.Right(95);
-        delay(1000);
-        An = Auto.distanz(true);
-        if(An < 15)
-        {
-          Auto.Right(50);
-        }
-      }
+  An = Auto.distanz(false);
+  if(An > 300)
+  {
+    Auto.setInterval(1000);
+  }
+  if(An > 200)
+  {
+    Auto.setInterval(400);
+  }
+  if(An > 100)
+  {
+    Auto.setInterval(100);
+  }
+  if(An < 15)
+  {
+    Auto.setInterval(50);
+  }
+  if(An >= 15)
+  {
+    Auto.Forwards();
+  }else{
+    Links = 0;
+    while(An < 15 && Links < 90)
+    {      
+      Auto.Left(5);
+      An = Auto.distanz(false);
+      Links++;
     }
-    
+    An = Auto.distanz(true);
+    while(An < 15)
+    {      
+      Auto.Right(5);
+      An = Auto.distanz(false);
+    }
+  }
 }
